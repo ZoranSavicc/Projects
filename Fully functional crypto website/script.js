0103d9c553410c54bf71b7acd6e1cdd363dd5b76
Zoran <zoran.savic241@gmail.com>
2022-06-02 17:02:27 +0200

    faq section. javascript toggle

--- Fully functional crypto website/script.js
+++ Fully functional crypto website/script.js
@@ -1,5 +1,21 @@
 //Navbar style change on scroll
-
 window.addEventListener('scroll', () =>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
-})
+});
+
+//Faq show/hide
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = 'uil uil-minus';
+        }
+        else{
+            icon.className = 'uil uil-plus';
+        }
+    })
+});
